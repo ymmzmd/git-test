836588b5a1ce994d404c78d02f77e8940266f248
袁东方 <ymmzm@foxmail.com>
2022-12-18 04:19:16 +0000

    gitee update commit
    Signed-off-by: 袁东方 <ymmzm@foxmail.com>

--- src/main/java/com/it/Gittest.java
+++ src/main/java/com/it/Gittest.java
@@ -7,5 +7,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("gittest");
         System.out.println("giteee");
+        System.out.println("giteee22222");
     }
 }
